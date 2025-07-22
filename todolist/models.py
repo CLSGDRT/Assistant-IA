99ad2e265852c156d468050aabdc661166da4e96
CLSGDRT <cls.godart@gmail.com>
2025-07-22 09:58:45 +0200

    todolist(creation):paramétrage de l'app

--- todolist/models.py
+++ todolist/models.py
@@ -2,3 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(
+        'auth.User',
+        on_delete = models.CASCADE,
+        related_name = 'tasks'
+    )
+
+    def __str__(self):
+        return self.title
